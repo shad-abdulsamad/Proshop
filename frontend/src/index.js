9a4bfc383d2281953070b0324d77b4c1d90dea49
Shad Abdulsamad <shad.A.A2002@gmail.com>
2023-09-09 19:23:09 +0300

    frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import App from './App';
+import ReactDOM from 'react-dom/client';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App/>}>
+      <Route index={true} path='/' element={<HomeScreen/>} />
+      <Route path='/product/:id' element={<ProductScreen/>}/>
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+reportWebVitals();
