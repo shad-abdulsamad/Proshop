f84cd4cbc382591415eed60fa530ed9ecd6627f0
Shad Abdulsamad <shad.A.A2002@gmail.com>
2023-09-10 14:19:07 +0300

    setting the backend

---
+++ backend/data/products.js
@@ -0,0 +1,88 @@
+const products = [
+    {
+        _id:1,
+        name: "Airpods Wireless Bluetooth Headphones",
+        image: '/images/airpods.jpg',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, ad blanditiis vitae eveniet, odio labore, laborum quasi veniam non omnis magni. Sapiente eligendi repellat dolorum, quibusdam modi itaque accusamus similique.',
+        brand: 'apple',
+        category: 'Electronics',
+        price:89.99,
+        countInStock: 9,
+        rating: 3,
+        numReviews: 12
+
+    },
+
+    {
+        _id:2,
+        name: "Cannon EOS 80D DSLR Camera",
+        image: '/images/camera.jpg',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, ad blanditiis vitae eveniet, odio labore, laborum quasi veniam non omnis magni. Sapiente eligendi repellat dolorum, quibusdam modi itaque accusamus similique.',
+        brand: 'cannon',
+        category: 'Electronics',
+        price:929.99,
+        countInStock: 8,
+        rating: 4.5,
+        numReviews: 17
+
+    },
+
+    {
+        _id:3,
+        name: "Logitech G-Series Gaming Mouse",
+        image: '/images/mouse.jpg',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, ad blanditiis vitae eveniet, odio labore, laborum quasi veniam non omnis magni. Sapiente eligendi repellat dolorum, quibusdam modi itaque accusamus similique.',
+        brand: 'Logitech',
+        category: 'Electronics',
+        price:49.99,
+        countInStock: 8,
+        rating: 4,
+        numReviews: 23
+
+    },
+
+    {
+        _id:4,
+        name: "iPhone 11 Pro 256GB Memory",
+        image: '/images/phone.jpg',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, ad blanditiis vitae eveniet, odio labore, laborum quasi veniam non omnis magni. Sapiente eligendi repellat dolorum, quibusdam modi itaque accusamus similique.',
+        brand: 'Apple',
+        category: 'Electronics',
+        price:599.99,
+        countInStock: 8,
+        rating: 4.5,
+        numReviews: 29
+
+    },
+
+    {
+        _id:5,
+        name: "Sony Playstation 5 White",
+        image: '/images/playstation.jpg',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, ad blanditiis vitae eveniet, odio labore, laborum quasi veniam non omnis magni. Sapiente eligendi repellat dolorum, quibusdam modi itaque accusamus similique.',
+        brand: 'Sony',
+        category: 'Electronics',
+        price:499.99,
+        countInStock: 0,
+        rating: 5,
+        numReviews: 30
+
+    },
+
+    {
+        _id:6,
+        name: "Amazon Echo Dot 3rd Generation",
+         image: '/images/alexa.jpg',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, ad blanditiis vitae eveniet, odio labore, laborum quasi veniam non omnis magni. Sapiente eligendi repellat dolorum, quibusdam modi itaque accusamus similique.',
+        brand: 'Amazon',
+        category: 'Electronics',
+        price:29.99,
+        countInStock: 8,
+        rating: 3,
+        numReviews: 5
+
+    },
+
+];
+
+export default products;
