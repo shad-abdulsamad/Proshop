f59c267d53ab0315034c246f875e89629efa0303
Shad Abdulsamad <shad.A.A2002@gmail.com>
2023-09-15 23:38:27 +0300

    connect with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js'
 import cors from 'cors';
 
 const port =5000;
+connectDB();
 
 const app = express();
 
@@ -27,3 +31,5 @@ app.listen(port, function(){
     console.log(`The server is running on port ${port}`);
 
 });
+
+// https://github.com/shad-abdulsamad/Proshop-v3.git
