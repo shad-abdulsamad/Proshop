ba28429d6a4ff3cbc2b3633a145d59986cad7e7e
Shad Abdulsamad <shad.A.A2002@gmail.com>
2023-10-05 17:03:09 +0300

    logout and clear cookies

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,7 +36,12 @@ const registerUser = asyncHandler(async(req, res)=>{
 });
 
 const logoutUser = asyncHandler(async(req, res)=>{
-    res.send('logout user');
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+
+    res.status(200).json({message: 'Logged Out'})
 });
 
 const getUserProfile = asyncHandler(async(req, res)=>{
