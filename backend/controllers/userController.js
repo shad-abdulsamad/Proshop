b1ffde3865f5becb1e41b96419f2786dcd5cbb73
Shad Abdulsamad <shad.A.A2002@gmail.com>
2023-10-05 15:48:41 +0300

    created the auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -7,6 +8,17 @@ const loginUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
+    const token = user.sign({userId: user._id}, process.env.JWT_SECRET,{
+        expiresIn:'30d'
+    });
+
+    //set JWT-token an HTTP cookie only
+    res.token('jwt', token,{
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+    });
     res.status(200).json({
       _id: user._id,
       name: user.name,
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -17,6 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(cors());
+
+//cookie parser middleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("API is running");
